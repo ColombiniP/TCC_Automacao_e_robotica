bd889dcdfe3854889bddbd4fc34e7d0ea338e8d0
ColombiniP <paschoal.colombini@live.com>
2025-09-23 18:23:02 -0300

    Resetando branch main para começar do zero

--- models/alunos.py
+++
@@ -1,8 +0,0 @@
-class Alunos:
-    def __init__(self, name, last_name, cpf, email, birth_day, disciplina):
-        self.name = name
-        self.last_name = last_name
-        self.cpf = cpf
-        self.email = email
-        self.birth_day = birth_day
-        self.disciplina = disciplina
--- pages/2_Lista_de_Alunos.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="LISTA DE ALUNO",page_icon="📄")
-
-st.title('📄 Lista do aluno')
