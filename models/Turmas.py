b2631446ebc80b1c93a2298cc5ed87a2cbedc993
ColombiniP <paschoal.colombini@live.com>
2025-09-25 16:54:49 -0300

    feat: arquivos de classe e controladores

---
+++ models/Turmas.py
@@ -0,0 +1,10 @@
+class Turmas:
+
+    def __init__(self, nome, cpf, email, aniversario, disciplina,consentimento):
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.aniversario = aniversario
+        self.disciplina = disciplina
+        self.consentimento = consentimento
+
